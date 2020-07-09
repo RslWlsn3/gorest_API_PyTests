0dd60a488e0071a065d71ba95ac81bed199581b9
CMote <connor.mote@intimetec.com>
2020-07-09 14:53:36 -0600

    All happy working

--- conftest.py
+++ conftest.py
@@ -3,16 +3,19 @@ import pytest
 import json
 from random import randrange
 
+SampleId = None
+####GET##########################################################################################################################################
 @pytest.fixture(scope="module")
 def get_request_returns_response():    
     response = requests.get("https://gorest.co.in/public-api/users", headers={'Authorization': 'Bearer VqRUbB84gJ6bMP97UeRK3MYpC608ZRcsVVYd'})
     return response
 
 @pytest.fixture(scope="module")
-def get_convert_response_to_json(get_request_returns_response):    
+def get_convert_response_to_json(get_request_returns_response):        
     response_body = get_request_returns_response.json()
     return response_body
 
+####POST#########################################################################################################################################
 POST_FIRST_NAME = "Connor"
 POST_LAST_NAME = "Mote"
 POST_GENDER = "male"
@@ -32,4 +35,55 @@ def post_request_returns_response():
 @pytest.fixture(scope="module")
 def post_get_convert_response_to_json(post_request_returns_response):    
     response_body = post_request_returns_response.json()
+    return response_body
+
+###PUT###########################################################################################################################################
+
+@pytest.fixture(scope="module")
+def return_PutData_obj():
+    class PutData:
+        PUT_FIRST_NAME = "Connor Mote"        
+        payload = {
+        "first_name":PUT_FIRST_NAME    
+        }
+        def __init__(self):
+            response = requests.get("https://gorest.co.in/public-api/users", headers={'Authorization': 'Bearer VqRUbB84gJ6bMP97UeRK3MYpC608ZRcsVVYd'})
+            response_body = response.json()
+            self.ID = response_body["result"][0]['id']
+
+    pd = PutData()
+    return pd
+
+@pytest.fixture(scope="module")
+def put_request_returns_response(return_PutData_obj):            
+    response = requests.put("https://gorest.co.in/public-api/users/" + str(return_PutData_obj.ID),
+        headers={'Authorization': 'Bearer VqRUbB84gJ6bMP97UeRK3MYpC608ZRcsVVYd'}, data = return_PutData_obj.payload)        
+    return response
+
+@pytest.fixture(scope="module") 
+def put_get_convert_response_to_json(put_request_returns_response):    
+    response_body = put_request_returns_response.json()
+    return response_body
+
+##DELETE###########################################################################################################################################
+
+@pytest.fixture(scope="module")
+def return_DELETEData_obj():
+    class DELETEData:
+        def __init__(self):
+            response = requests.get("https://gorest.co.in/public-api/users", headers={'Authorization': 'Bearer VqRUbB84gJ6bMP97UeRK3MYpC608ZRcsVVYd'})
+            response_body = response.json()
+            self.ID = response_body["result"][1]['id']
+
+    dd = DELETEData()
+    return dd
+
+@pytest.fixture(scope="module")
+def delete_request_returns_response(return_DELETEData_obj):    
+    response = requests.delete("https://gorest.co.in/public-api/users/" + return_DELETEData_obj.ID, headers={'Authorization': 'Bearer VqRUbB84gJ6bMP97UeRK3MYpC608ZRcsVVYd'})        
+    return response
+
+@pytest.fixture(scope="module")
+def delete_get_convert_response_to_json(delete_request_returns_response):    
+    response_body = delete_request_returns_response.json()
     return response_body
--- test_gorestAPI.py
+++ test_gorestAPI.py
@@ -15,7 +15,7 @@ class TestGetHappy:
     def test_get_results_return_twenty(self, get_convert_response_to_json):
         assert len(get_convert_response_to_json["result"]) == 20
 
-####POST######################################################################################################################################################
+# ####POST######################################################################################################################################################
 POST_FIRST_NAME = "Connor"
 POST_LAST_NAME = "Mote"
 POST_GENDER = "male"
@@ -36,56 +36,31 @@ class TestPostHappy:
         assert post_get_convert_response_to_json["result"][key] == value
 
 ###PUT#######################################################################################################################################################
-# PUT_FIRST_NAME = "Connor"
-# PUT_ID = "1859"
-# payload = {
-#     "first_name":PUT_FIRST_NAME
-# }
 
-# @pytest.fixture(scope="module")
-# def put_request_returns_response():    
-#     response = requests.put("https://gorest.co.in/public-api/users/" + PUT_ID, headers={'Authorization': 'Bearer VqRUbB84gJ6bMP97UeRK3MYpC608ZRcsVVYd'}, data = payload)        
-#     return response
+@pytest.mark.puthappy
+class TestPutHappy:
+    def test_get_check_status_code_equals_200(self, put_request_returns_response, put_get_convert_response_to_json):
+        assert put_request_returns_response.status_code == 200 #need to flip
+        assert put_get_convert_response_to_json["_meta"]["code"] == 200
 
-# @pytest.fixture(scope="module") #put fixtures into one file
-# def put_get_convert_response_to_json(put_request_returns_response):    
-#     response_body = put_request_returns_response.json()
-#     return response_body
+    def test_put_check_content_type_equals_json(self, put_request_returns_response):
+        x =  put_request_returns_response.headers['Content-Type']
+        assert x == "application/json; charset=UTF-8"
+        y = 1
 
-# def test_get_check_status_code_equals_200(put_request_returns_response, put_get_convert_response_to_json):
-#     assert put_request_returns_response.status_code == 200
-#     assert put_get_convert_response_to_json["_meta"]["code"] == 200
-
-# def test_put_check_content_type_equals_json(put_request_returns_response):
-#     assert put_request_returns_response.headers['Content-Type'] == "application/json; charset=UTF-8"
-
-# @pytest.mark.parametrize("key,value", [ ('id', PUT_ID), ('first_name', PUT_FIRST_NAME), ('last_name', "Brandy")])
-# def test_get_payload(key, value, put_get_convert_response_to_json):   
-#     assert put_get_convert_response_to_json["result"][key] == value
+    def test_get_payload(self, put_get_convert_response_to_json, return_PutData_obj):   
+        assert put_get_convert_response_to_json["result"]['id'] == return_PutData_obj.ID
+        assert put_get_convert_response_to_json["result"]['first_name'] == return_PutData_obj.PUT_FIRST_NAME
 
 # ##DELETE#####################################################################################################################################################
-# DELETE_ID = "1883" #need to change every time or create a POST during tear down
+@pytest.mark.deletehappy
+class TestDeleteHappy:
+    def test_get_check_status_code_equals_200(self, delete_request_returns_response, delete_get_convert_response_to_json):
+        assert delete_request_returns_response.status_code == 200
+        assert delete_get_convert_response_to_json["_meta"]["code"] == 204
 
-# @pytest.fixture(scope="module")
-# def delete_request_returns_response():    
-#     response = requests.delete("https://gorest.co.in/public-api/users/" + DELETE_ID, headers={'Authorization': 'Bearer VqRUbB84gJ6bMP97UeRK3MYpC608ZRcsVVYd'})        
-#     return response
+    def test_delete_check_content_type_equals_json(self, delete_request_returns_response):
+        assert delete_request_returns_response.headers['Content-Type'] == "application/json; charset=UTF-8"
 
-# @pytest.fixture(scope="module")
-# def delete_get_convert_response_to_json(delete_request_returns_response):    
-#     response_body = delete_request_returns_response.json()
-#     return response_body
-
-# def test_get_check_status_code_equals_200(delete_request_returns_response, delete_get_convert_response_to_json):
-#     assert delete_request_returns_response.status_code == 200
-#     assert delete_get_convert_response_to_json["_meta"]["code"] == 204
-
-# def test_delete_check_content_type_equals_json(delete_request_returns_response):
-#     assert delete_request_returns_response.headers['Content-Type'] == "application/json; charset=UTF-8"
-
-# def test_get_payload(delete_get_convert_response_to_json):   
-#     assert delete_get_convert_response_to_json["result"] == None
-
-# def test_basic():
-#     print("hello")
-#     assert 2 == 2
+    def test_get_payload(self, delete_get_convert_response_to_json):   
+        assert delete_get_convert_response_to_json["result"] == None
