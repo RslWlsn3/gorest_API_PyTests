2fd02ecc5013024821b67b7f21bce00e0a43e317
CMote <connor.mote@intimetec.com>
2020-07-10 14:41:50 -0600

    Added negative tests

--- conftest.py
+++ conftest.py
@@ -5,6 +5,7 @@ from random import randrange
 
 
 # ###GET##################################################################
+# happy
 @pytest.fixture(scope="module")
 def get_request_returns_response():
     response = requests.get(
@@ -18,6 +19,17 @@ def get_request_returns_response():
 def get_convert_response_to_json(get_request_returns_response):
     response_body = get_request_returns_response.json()
     return response_body
+
+# negative
+
+
+@pytest.fixture(scope="module")
+def get_bad_request():
+    response = requests.get(
+        "https://gorest.co.in/public-api/users/10193411294",
+        headers={
+            'Authorization': 'Bearer VqRUbB84gJ6bMP97UeRK3MYpC608ZRcsVVYd'})
+    return response
 
 
 # ###POST#################################################################
@@ -44,12 +56,39 @@ def post_request_returns_response():
 
 
 @pytest.fixture(scope="module")
-def post_get_convert_response_to_json(post_request_returns_response):
+def post_convert_response_to_json(post_request_returns_response):
     response_body = post_request_returns_response.json()
     return response_body
 
+# negative
+
+
+@pytest.fixture(scope="module")
+def negative_post_request_returns_response(get_convert_response_to_json):
+    already_used_email = get_convert_response_to_json["result"][0]["email"]
+    payload = {
+        "first_name": POST_FIRST_NAME,
+        "last_name": POST_LAST_NAME,
+        "gender": POST_GENDER,
+        "email": already_used_email
+    }
+    response = requests.post(
+        "https://gorest.co.in/public-api/users",
+        headers={
+            'Authorization': 'Bearer VqRUbB84gJ6bMP97UeRK3MYpC608ZRcsVVYd'},
+        data=payload)
+    return response
+
+
+@pytest.fixture(scope="module")
+def negative_post_request_returns_json(negative_post_request_returns_response):
+    response_body = negative_post_request_returns_response.json()
+    return response_body
 
 # ##PUT###################################################################
+# happy
+
+
 @pytest.fixture(scope="module")
 def return_PutData_obj():
     class PutData:
@@ -83,12 +122,32 @@ def put_request_returns_response(return_PutData_obj):
 
 
 @pytest.fixture(scope="module")
-def put_get_convert_response_to_json(put_request_returns_response):
+def put_convert_response_to_json(put_request_returns_response):
     response_body = put_request_returns_response.json()
+    return response_body
+
+# negative
+
+
+@pytest.fixture(scope="module")
+def negative_put_request_returns_response(return_PutData_obj):
+    response = requests.put(
+        "https://gorest.co.in/public-api/users/1092029810",
+        headers={
+            'Authorization': 'Bearer VqRUbB84gJ6bMP97UeRK3MYpC608ZRcsVVYd'},
+        data=return_PutData_obj.payload)
+    return response
+
+
+@pytest.fixture(scope="module")
+def negative_put_convert_response_to_json(
+        negative_put_request_returns_response):
+    response_body = negative_put_request_returns_response.json()
     return response_body
 
 
 # ##DELETE##################################################################
+# happy
 @pytest.fixture(scope="module")
 def return_DELETEData_obj():
     class DELETEData:
@@ -116,6 +175,24 @@ def delete_request_returns_response(return_DELETEData_obj):
 
 
 @pytest.fixture(scope="module")
-def delete_get_convert_response_to_json(delete_request_returns_response):
+def delete_convert_response_to_json(delete_request_returns_response):
     response_body = delete_request_returns_response.json()
+    return response_body
+
+# negative
+
+
+@pytest.fixture(scope="module")
+def negative_delete_request_returns_response():
+    response = requests.delete(
+        "https://gorest.co.in/public-api/users/20974203928",
+        headers={
+            'Authorization': 'Bearer VqRUbB84gJ6bMP97UeRK3MYpC608ZRcsVVYd'})
+    return response
+
+
+@pytest.fixture(scope="module")
+def negative_delete_convert_response_to_json(
+        negative_delete_request_returns_response):
+    response_body = negative_delete_request_returns_response.json()
     return response_body
--- test_gorestAPI.py
+++ test_gorestAPI.py
@@ -1,10 +1,10 @@
 import pytest
 
 
-# happy paths
 # ###GET###################################################################
-@pytest.mark.gethappy
-class TestGetHappy:
+# happy paths
+@pytest.mark.get
+class TestGet:
     def test_get_check_status_code_equals_200(
             self, get_request_returns_response):
         assert 200 == get_request_returns_response.status_code
@@ -17,6 +17,14 @@ class TestGetHappy:
     def test_get_results_return_twenty(self, get_convert_response_to_json):
         assert 20 == len(get_convert_response_to_json["result"])
 
+# Negative testing
+    def test_get_check_status_code_equals_404(
+            self, get_bad_request):
+        assert 200 == get_bad_request.status_code
+        response_body = get_bad_request.json()
+        assert 404 == response_body["_meta"]["code"]
+        assert 404 == response_body["result"]["status"]
+
 
 # ####POST#################################################################
 POST_FIRST_NAME = "Connor"
@@ -24,14 +32,14 @@ POST_LAST_NAME = "Mote"
 POST_GENDER = "male"
 
 
-@pytest.mark.posthappy
-class TestPostHappy:
-    def test_get_check_status_code_equals_200(
+@pytest.mark.post
+class TestPost:
+    def test_check_status_code_equals_200(
             self,
             post_request_returns_response,
-            post_get_convert_response_to_json):
+            post_convert_response_to_json):
         assert 200 == post_request_returns_response.status_code
-        assert 200 == post_get_convert_response_to_json["_meta"]["code"]
+        assert 200 == post_convert_response_to_json["_meta"]["code"]
 
     def test_post_check_content_type_equals_json(
             self, post_request_returns_response):
@@ -45,52 +53,75 @@ class TestPostHappy:
                                POST_LAST_NAME),
                                  ('gender',
                                   POST_GENDER)])  # consts in separate file
-    def test_get_payload(self, key, value, post_get_convert_response_to_json):
-        assert None is not post_get_convert_response_to_json["result"]["id"]
-        assert value == post_get_convert_response_to_json["result"][key]
+    def test_post_payload(self, key, value, post_convert_response_to_json):
+        assert None is not post_convert_response_to_json["result"]["id"]
+        assert value == post_convert_response_to_json["result"][key]
 
+# Negative testing
+    def test_check_status_code_equals_422(
+            self,
+            negative_post_request_returns_response,
+            negative_post_request_returns_json):
+        assert 200 == negative_post_request_returns_response.status_code
+        assert 422 == negative_post_request_returns_json["_meta"]["code"]
 # ##PUT####################################################################
 
 
-@pytest.mark.puthappy
-class TestPutHappy:
-    def test_get_check_status_code_equals_200(
+@pytest.mark.put
+class TestPut:
+    def test_check_status_code_equals_200(
             self,
             put_request_returns_response,
-            put_get_convert_response_to_json):
+            put_convert_response_to_json):
         assert 200 == put_request_returns_response.status_code
-        assert 200 == put_get_convert_response_to_json["_meta"]["code"]
+        assert 200 == put_convert_response_to_json["_meta"]["code"]
 
     def test_put_check_content_type_equals_json(
             self, put_request_returns_response):
         content_type = put_request_returns_response.headers['Content-Type']
         assert "application/json; charset=UTF-8" == content_type
 
-    def test_get_payload(
+    def test_put_payload(
             self,
-            put_get_convert_response_to_json,
+            put_convert_response_to_json,
             return_PutData_obj):
         assert (return_PutData_obj.ID ==
-                put_get_convert_response_to_json["result"]['id'])
+                put_convert_response_to_json["result"]['id'])
         assert (return_PutData_obj.PUT_FIRST_NAME ==
-                put_get_convert_response_to_json["result"]['first_name'])
+                put_convert_response_to_json["result"]['first_name'])
+
+# Negative testing
+    def test_check_status_code_equals_404(
+            self,
+            negative_put_request_returns_response,
+            negative_put_convert_response_to_json):
+        assert 200 == negative_put_request_returns_response.status_code
+        assert 404 == negative_put_convert_response_to_json["_meta"]["code"]
 
 # ##DELETE#################################################################
 
 
-@pytest.mark.deletehappy
-class TestDeleteHappy:
-    def test_get_check_status_code_equals_200(
+@pytest.mark.delete
+class TestDelete:
+    def test_check_status_code_equals_200(
             self,
             delete_request_returns_response,
-            delete_get_convert_response_to_json):
+            delete_convert_response_to_json):
         assert 200 == delete_request_returns_response.status_code
-        assert 204 == delete_get_convert_response_to_json["_meta"]["code"]
+        assert 204 == delete_convert_response_to_json["_meta"]["code"]
 
     def test_delete_check_content_type_equals_json(
             self, delete_request_returns_response):
         assert ("application/json; charset=UTF-8" ==
                 delete_request_returns_response.headers['Content-Type'])
 
-    def test_get_payload(self, delete_get_convert_response_to_json):
-        assert None is delete_get_convert_response_to_json["result"]
+    def test_delete_payload(self, delete_convert_response_to_json):
+        assert None is delete_convert_response_to_json["result"]
+
+# Negative testing
+    def test_get_check_status_code_equals_404(
+            self,
+            negative_delete_request_returns_response,
+            negative_delete_convert_response_to_json):
+        assert 200 == negative_delete_request_returns_response.status_code
+        assert 404 == negative_delete_convert_response_to_json["_meta"]["code"]
